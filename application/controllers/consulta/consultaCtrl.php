403a9b066d4c942a3b7d46e09a23704ec9e8e575
holajorge <franciscomigueljorge>
2017-08-26 10:33:41 -0500

    dajksnkdjansd

--- application/controllers/consulta/consultaCtrl.php
+++ application/controllers/consulta/consultaCtrl.php
@@ -1,25 +1,28 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');	
+
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 class ConsultaCtrl extends CI_Controller {
 
-	public function __construct()
-	{
+    public function __construct() {
 
-		header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Origin: *');
         header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
         $method = $_SERVER['REQUEST_METHOD'];
-        if($method == "OPTIONS") {
+        if ($method == "OPTIONS") {
             die();
         }
-		parent::__construct();
-		//Do your magic here		
-	}
+        parent::__construct();
+        //Do your magic here		
+    }
 
-	public function index()
-	{
-		$this->load->view("consulta/consulta.html");
-	}
+    public function index() {
+        $this->load->view("consulta/consulta.html");
+    }
+    
+    
+    
+    
 
 }
