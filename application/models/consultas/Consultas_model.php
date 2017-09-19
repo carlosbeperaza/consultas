e8c415f10098c7721102f1c9f7209e8ce06d69e7
Carlos Be Peraza <correo_de_carlos@outlook.com>
2017-09-19 01:08:41 -0500

    Se agregaron nuevos modelos del js, nuevas consultas, controllers, models en consultas...

--- application/models/consultas/Consultas_model.php
+++ application/models/consultas/Consultas_model.php
@@ -1,19 +1,28 @@
 <?php
 class Consultas_model extends CI_Model {
 
-  public $GET_TIPO_CONSULTAS = "select * from tipo_consulta";
-  public $GET_FIELD_INFO_GENERAL = "select * from fields_info_general";
-  public $GET_FIELD_INFO_PACIENTE = "select * from field_datos_paciente";
-  public $GET_DATA_RELACION_TEMPO = "select * from relacion_temporal";
-  public $GET_DATA_DERECHOHABIENCIA = "select * from derechohabiencias";
-  public $GET_DATA_DISCAPACIDAD = "select * from discapacidades";
-  public $GET_DATA_CLAVE_PERSONA = "select * from clave_edad";
+  private $GET_TIPO_CONSULTAS = "select * from tipo_consulta";
+  private $GET_FIELD_INFO_GENERAL = "select * from fields_info_general";
+  private $GET_FIELD_INFO_PACIENTE = "select * from field_datos_paciente";
+  private $GET_DATA_RELACION_TEMPO = "select * from relacion_temporal";
+  private $GET_DATA_DERECHOHABIENCIA = "select * from derechohabiencias";
+  private $GET_DATA_DISCAPACIDAD = "select * from discapacidades";
+  private $GET_DATA_CLAVE_PERSONA = "select * from clave_edad";
+  private $GET_PROGRAMAS = "select * from programas where status = 1";
+  private $GET_IMC = "select * from imc";
+  private $GET_TIPO_EDI_PRIMERA_VEZ = "select * from edi_tipo where id_edi = 1 or id_edi = 2 or id_edi = 3";
+  private $GET_TIPO_EDI_SUBSECUENTE = "select * from edi_tipo where id_edi = 4 or id_edi = 5 or id_edi = 6";
+  private $GET_REFERIDO_BY = "select * from referido_by where status = 1";
+  private $GET_TIPO_EDI_ALL = "select * from edi_tipo";
+  private $GET_RESULTADO_BATTELLE = "select * from rest_battelle";
+  private $GET_COMPLICACIONES = "select * from complicaciones";
+  private $GET_OTRAS_ACCIONES = "select * from embarazo_acciones";
+  
   //aqui es donde se hace uso de la base datos "urgencias"
   public  function __construct()
   {
     parent::__construct();
     $this->load->database();
-
   }
 
   public function getTipoConsulta()
@@ -21,36 +30,91 @@ class Consultas_model extends CI_Model {
     $query = $this->db->query($this->GET_TIPO_CONSULTAS);
     return $query->result();     
   }
-
+  
   public function getFieldsInfoGeneral()
   {
       $query = $this->db->query($this->GET_FIELD_INFO_GENERAL);
       return $query->result();     
   }
-
+  
   public function getFieldsPaciente()
   {
     $query = $this->db->query($this->GET_FIELD_INFO_PACIENTE);
     return $query->result();     
   }
+  
   public function getDataRelacionTemporal(){
     $query = $this->db->query($this->GET_DATA_RELACION_TEMPO);
     return $query->result();    
   }
+  
   public function getDataDerechohabiencia()
   {
      $query = $this->db->query($this->GET_DATA_DERECHOHABIENCIA);
      return $query->result();  
   }
+  
   public function getDataDiscapacidad()
   {
      $query = $this->db->query($this->GET_DATA_DISCAPACIDAD);
      return $query->result();  
   }
+  
   public function getDataClavePersona()
   {
-
     $query = $this->db->query($this->GET_DATA_CLAVE_PERSONA);
+    return $query->result(); 
+  }
+  
+  public function getProgramas()
+  {
+    $query = $this->db->query($this->GET_PROGRAMAS);
+    return $query->result(); 
+  }
+  
+  public function getIMC()
+  {
+    $query = $this->db->query($this->GET_IMC);
+    return $query->result(); 
+  }
+  
+   public function getEdiTipoUno()
+  {
+    $query = $this->db->query($this->GET_TIPO_EDI_PRIMERA_VEZ);
+    return $query->result(); 
+  }
+  
+   public function getEdiTipoDos()
+  {
+    $query = $this->db->query($this->GET_TIPO_EDI_SUBSECUENTE);
+    return $query->result(); 
+  }
+  
+  public function getReferidoBy()
+  {
+    $query = $this->db->query($this->GET_REFERIDO_BY);
+    return $query->result(); 
+  }
+  
+  public function getResultBattelle()
+  {
+    $query = $this->db->query($this->GET_RESULTADO_BATTELLE);
+    return $query->result(); 
+  }
+  
+  public function getEdiTipoAll()
+  {
+    $query = $this->db->query($this->GET_TIPO_EDI_ALL);
+    return $query->result(); 
+  }
+  public function getComplicaciones()
+  {
+    $query = $this->db->query($this->GET_COMPLICACIONES);
+    return $query->result(); 
+  }
+  public function getOtrasAcciones()
+  {
+    $query = $this->db->query($this->GET_OTRAS_ACCIONES);
     return $query->result(); 
   }
 
--- resources/js/services/consultas/consultasService.js
+++ resources/js/services/consultas/consultasService.js
@@ -11,11 +11,11 @@ app.factory('consultasService', ['$http',
                     return response.data;
                 });
             },
-            getFieldsPaciente: function () {
-                return $http.get("consultas/consultasCtrl/getFieldsPaciente").then(function (response) {
-                    return response.data;
-                });
-            },
+//            getFieldsPaciente: function () {
+//                return $http.get("consultas/consultasCtrl/getFieldsPaciente").then(function (response) {
+//                    return response.data;
+//                });
+//            },
              getDataRelacionTemporal: function () {
                 return $http.get("consultas/consultasCtrl/getDataRelacionTemporal").then(function (response) {
                     return response.data;
@@ -33,6 +33,51 @@ app.factory('consultasService', ['$http',
             },
             getDataClavePersona: function () {
                 return $http.get("consultas/consultasCtrl/getDataClavePersona").then(function (response) {
+                    return response.data;
+                });
+            },
+            getProgramas: function () {
+                return $http.get("consultas/consultasCtrl/getProgramas").then(function (response) {
+                    return response.data;
+                });
+            },            
+            getIMC: function () {
+                return $http.get("consultas/consultasCtrl/getIMC").then(function (response) {
+                    return response.data;
+                });
+            },
+            getEdiTipoUno: function () {
+                return $http.get("consultas/consultasCtrl/getEdiTipoUno").then(function (response) {
+                    return response.data;
+                });
+            },
+            getEdiTipoDos: function () {
+                return $http.get("consultas/consultasCtrl/getEdiTipoDos").then(function (response) {
+                    return response.data;
+                });
+            },
+            getReferidoBy: function () {
+                return $http.get("consultas/consultasCtrl/getReferidoBy").then(function (response) {
+                    return response.data;
+                });
+            },
+            getResultBattelle: function () {
+                return $http.get("consultas/consultasCtrl/getResultBattelle").then(function (response) {
+                    return response.data;
+                });
+            },
+            getEdiTipoAll: function () {
+                return $http.get("consultas/consultasCtrl/getEdiTipoAll").then(function (response) {
+                    return response.data;
+                });
+            },
+            getComplicaciones: function () {
+                return $http.get("consultas/consultasCtrl/getComplicaciones").then(function (response) {
+                    return response.data;
+                });
+            },
+             getOtrasAcciones: function () {
+                return $http.get("consultas/consultasCtrl/getOtrasAcciones").then(function (response) {
                     return response.data;
                 });
             }
