eb4fbf43090945c152d8160347e4e52573c1ad41
Micheal Philip Strand <micky_mike90@hotmail.com>
2017-09-19 17:39:40 -0500

    fix para que no redireccion al login al hacer click en una pestana

--- resources/js/controllers/mainCtrl.js
+++ resources/js/controllers/mainCtrl.js
@@ -3,28 +3,39 @@
  * Clip-Two Main Controller
  */
 app.controller('AppCtrl', ['$rootScope', '$scope', '$state', '$location', '$auth', '$http',
-    function ($rootScope, $scope, $state, $location, $auth, $http) {
-        
-        $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
-//            event.preventDefault();
-//            if ($auth.isAuthenticated() && toState && toState !== fromState) {
-//                 $location.path(toState.url);
-//            }else{
-//                $location.path('/login'); 
-//            } 
-        });
+    function ($rootScope, $scope, $state, $location, $auth, $http) {        
+
         $scope.logout = function (event) {
             if (window.confirm("logout?") === true) {
                 $auth.removeToken();
                 $auth.logout();
                 $location.path('/login');  
             } 
-             
-               
-            //$auth.removeToken();
-            //$auth.logout();
-            //$location.path('/login/signin');
         };
+//         if ($auth.isAuthenticated() && toState && toState !== fromState) {
+
         
 }]);
+
+app.run(function ($rootScope, $location,$auth) {
+
+    // enumerate routes that don't need authentication
+    var routesThatDontRequireAuth = ['/login'];
+
+    // check if current location matches route  
+    var routeClean = function (route) {
+      return find(routesThatDontRequireAuth,
+        function (noAuthRoute) {
+          return startsWith(route, noAuthRoute);
+        });
+    };
+
+    $rootScope.$on('$stateChangeStart', function (ev, to, toParams, from, fromParams) {
+      // if route requires auth and user is not logged in
+      if (!routeClean($location.url()) && ! $auth.isAuthenticated()) {
+        // redirect back to login
+        $location.path('/login');
+      }
+    });
+});
 
